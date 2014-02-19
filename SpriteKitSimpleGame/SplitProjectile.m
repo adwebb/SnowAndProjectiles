8de150e8cfb24b487b5606370fe26e1432e22f5b
Andrew Webb <adwebb@gmail.com>
2014-02-19 15:54:20 -0600

    working on split projectile

--- SpriteKitSimpleGame/SplitProjectile.m
+++ SpriteKitSimpleGame/SplitProjectile.m
@@ -11,6 +11,17 @@
 @implementation SplitProjectile
 +(SplitProjectile*)splitProjectileOfRank:(int)rank
 {
-    return nil;
+    
+    
+    SplitProjectile* splitProjectile1 = [SplitProjectile spriteNodeWithImageNamed:@"snowball"];
+    splitProjectile1.size = CGSizeMake(splitProjectile1.size.width/3, splitProjectile1.size.height/3);
+    splitProjectile1.color = [SKColor colorWithRed:177/255.0 green:198/255.0 blue:0 alpha:1];
+    splitProjectile1.colorBlendFactor = .5;
+    splitProjectile1.damage = 1;
+    splitProjectile1 = (SplitProjectile*)[super setProjectileProperties:splitProjectile1];
+    
+    
+    
+    return splitProjectile1;
 }
 @end
